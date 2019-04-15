a2d87c30413ae11dc8926d5339a10d910ca0dcde
prakash1ranjan <48158643+prakash1ranjan@users.noreply.github.com>
2019-04-15 11:08:06 +0530

    Update init.c

--- src/init.c
+++ src/init.c
@@ -14,12 +14,15 @@ int init(struct data *sol){
 	int l_ar=0;
 	double initcond,bc_xl,bc_xr,bc_yb,bc_yt;
 	//sol = (struct data *)malloc(sizeof(struct data));
-	sol->nx = 100+2; //+2 for boundary condition 
-	sol->ny = 100+2; 
+	sol->nx = 100;
+	sol->ny = 100; 
 	sol->t = 0;
 	sol->told = 0;
-	sol->dt = 0.0001;
-	int size = sizeof(double) * sol->nx * sol->ny;
+	sol->dt = 0.001;
+    
+    	int N = (sol->nx) * (sol->ny);
+	int size = sizeof(double) * N;
+    
 	sol->u = (double *)malloc(size);
 	sol->uold = (double *)malloc(size);
 	sol->rhs = (double *)malloc(size);
@@ -27,21 +30,27 @@ int init(struct data *sol){
 	sol->y = (double *)malloc(size);
 	sol->IC = (double *)malloc(size);
 
-	int N = sol->nx * sol->ny;
+
 
 	int ix,iy;
 	double dx=(double)1/(nx-1),dy=(double)1/(ny-1);
+    
+    	printf("dx: %f ", dx);
+ 
+        /* Manual Input has been toggled off for now to minimize time during test
 	printf("\n Provide boundary condition at x=0\n");
 	scanf("%lf", &bc_xl);
-	printf("\n Provide boundary condition at x=l\n");
+	printf("\n Provide boundary condition at x=1\n");
 	scanf("%lf", &bc_xr);
 	printf("\n Provide boundary condition at y=0\n");
 	scanf("%lf", &bc_yb);
-	printf("\n Provide boundary condition at y=l\n");
+	printf("\n Provide boundary condition at y=1\n");
 	scanf("%lf", &bc_yt);
-	printf("\n Provide initialization condition \n");
-	scanf("%lf", &initcond);
+//	printf("\n Provide initialization condition \n");
+//	scanf("%lf", &initcond);
 
+*/
+        
 	// init grid, u, uold, rhs
 	for(int i=0;i<N; ++i){
 		ix = i / ny;
@@ -51,21 +60,35 @@ int init(struct data *sol){
 		sol->u[i] = 0;
 		sol->uold[i] = 0;
 		sol->rhs[i] = 0;
-		sol->IC[i] = initcond;
 	}
-	//BC for left boundary xl
-	for(int i=0;i<ny;++i)
+
+		sol->u[4949] = 1;      // Initial condition as a point source
+
+        
+        
+        
+	/*  Manual Input has been toggled off for now to minimize time during test
+	//BC for Top boundary xl
+	for(int i=0;i<=2*ny;++i)
 	{sol->u[i] = bc_xl; } //first nx elements of the array
-	//BC for right boundary xr
-	l_ar=N-ny;
+	
+	
+	//BC for Bottom boundary xr
+	l_ar=N-2ny;
 	for(int i=l_ar;i<N;++i)
 	{sol->u[i] = bc_xr; } //last nx elements of the array
+	
+	
 	//BC for bottom boundary yb
 	for(int i=0;i<nx;++i)
 	{sol->u[i*ny] = bc_yb; } //Every nx_th element represent first column element
+	
+	
 	//BC for top boundary yt
 	for(int i=0;i<nx;++i)
 	{sol->u[(i+1)*ny-1] = bc_yt; } //Every nx-1 element represent last column element
+
+	*/
 
 	return 0;
 }
