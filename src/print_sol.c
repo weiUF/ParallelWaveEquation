7f2f55cc683586d490d98f59ead367488b4dc645
prakash1ranjan <48158643+prakash1ranjan@users.noreply.github.com>
2019-04-15 11:19:21 +0530

    Updated print_sol.c; Output to a file; Validated

--- src/print_sol.c
+++ src/print_sol.c
@@ -1,23 +1,42 @@
 #include <stdio.h>
 #include "data.h"
+#include <stdlib.h> 
 
 int print_sol(struct data *s){
 	
 	int idx,i,j;
+    double time = s->t;
+    char filename[300];
+    
+    sprintf(filename, "Solution_U_at_t_%f_sec.txt", time);
 
-	printf("dimension\n");
+    FILE *out_file = fopen(filename, "w"); // write only 
+
+    
+    if (out_file == NULL || out_file == NULL) 
+            {   
+              printf("Error! Could not open file\n"); 
+              exit(-1); // must include stdlib.h 
+            }
+            
+    /*        
+	printf("%s\n","dimension");
 	printf("nx: %d ", s->nx);
 	printf("ny: %d\n", s->ny);
-	printf("current time: %f\n", s->t);
 
 	printf("%s\n","solution u:");
+	
+	*/
+    
 	for (i=0;i<s->nx;++i){
 		for (j=0;j<s->ny;++j){
 			idx = i * s->nx + j;
-			printf("%f ", s->u[idx]);
+            fprintf(out_file, "%f,",s->u[idx]); // write to file
 		}
-		printf("%s\n","");
+        fprintf(out_file, "\n"); // write to file
 	}
-
+    
+    time = time + 0.001;
+    
 	return 0;
 }
